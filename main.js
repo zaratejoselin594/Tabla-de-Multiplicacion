c05ba1ff72d3dbea4c87dde1c4c588c7da865360
Joselin Zarate <zaratejoselin594@gmail.com>
2023-07-23 20:01:42 -0300

    tabla de multiplicar

--- main.js
+++ main.js
@@ -3,26 +3,25 @@ let res = document.querySelectorAll(".resultado")
 const btn = document.querySelector(".boton");
 const inp = document.querySelector(".input");
 
+//Le damos un valor por defecto para prevenir que se calcule sin contener valor el input
+inp.value = 0;
+
 //Abrimos escucha al evento click
 btn.addEventListener("click",(e) => {
   e.preventDefault();
-  //detectamos que contenga numero el input
-  if (inp.value.length == 0) {
-    alert("Selecciona un número...");
-  } else {
-    // Hacemos que todos los p contengan el numero ingresado
-    num.forEach(n => {
-      n.textContent = inp.value; //mostramos el resultado
-    });
+  // Hacemos que todos los p contengan el numero ingresado
+  num.forEach(n => {
+    n.textContent = inp.value; //mostramos el resultado
+  });
 
-    //iniciamos mult que sera el numero con el que vamos a multiplicar el numero ingresado
-    let mult = 0
+  //iniciamos mult que sera el numero con el que vamos a multiplicar el numero ingresado
+  let mult = 0
 
-    res.forEach(r => { 
-      mult++
-      //Multiplicaremos el numero ingresado por el valor de mult de cada vuelta
-      let result = inp.value * mult;
-      r.textContent = result;  //mostramos el resultado
-    });  
-  }
+  res.forEach(r => { 
+    mult++
+    //Multiplicaremos el numero ingresado por el valor de mult de cada vuelta
+    let result = inp.value * mult;
+    r.textContent = result;  //mostramos el resultado
+  });  
+  
 })
